70862aa9d37bdf40203bfb29a1e49ed66cde4834
Blaq Swan <mr.alangobryan@gmail.com>
2023-04-20 18:25:14 +0300

    rename

---
+++ intro/exercises/10.go
@@ -0,0 +1,13 @@
+//usr/bin/true; exec /usr/bin/env go run $0 $@
+
+// A program that prints numbers 1 through 10
+
+package main
+
+import "fmt"
+
+func main(){
+	for i := 0; i <= 10; i++ {
+		fmt.Printf("%d \n", i)
+	}
+}
